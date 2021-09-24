1c42dfc239b8845b2ccb5185dbd5930305ffa011
Christopher Reeve <reeveworkmail@gmail.com>
2021-09-24 14:37:36 +0700

    update LICENSE.md

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,7 +10,12 @@
 			<h1 class="font-bold text-7xl text-center mb-5">Vitailse</h1>
 			<p>
 				Welcome to Vitailse, Vite starter template for building an admin
-				template from scratch with <a href="https://tailwindcss.com/" class="text-blue-500 hover:underline">TailwindCSS</a>
+				template from scratch with
+				<a
+					href="https://tailwindcss.com/"
+					class="text-blue-500 hover:underline"
+					>TailwindCSS</a
+				>
 			</p>
 		</div>
 	</div>
