20372f09e029cf8f58f97460e9fe315296888bbe
Christopher Reeve <reeveworkmail@gmail.com>
2021-10-20 20:59:06 +0700

    add editor config

--- src/modules/i18n.ts
+++ src/modules/i18n.ts
@@ -6,22 +6,22 @@ import { createI18n } from 'vue-i18n';
 
 // Don't need this? Try vitesse-lite: https://github.com/antfu/vitesse-lite
 const messages = Object.fromEntries(
-	Object.entries(import.meta.globEager('../../locales/*.{y(a)?ml,json}')).map(
-		([key, value]) => {
-			const isYamlOrJson = key.endsWith('.yaml') || key.endsWith('.json');
+  Object.entries(import.meta.globEager('../../locales/*.{y(a)?ml,json}')).map(
+    ([key, value]) => {
+      const isYamlOrJson = key.endsWith('.yaml') || key.endsWith('.json');
 
-			return [key.slice(14, isYamlOrJson ? -5 : -4), value.default];
-		}
-	)
+      return [key.slice(14, isYamlOrJson ? -5 : -4), value.default];
+    }
+  )
 );
 
 export const install: ViteSetupModule = ({ app }) => {
-	const i18n = createI18n({
-		legacy: false,
-		locale: 'en',
-		messages,
-		globalInjection: true,
-	});
+  const i18n = createI18n({
+    legacy: false,
+    locale: 'en',
+    messages,
+    globalInjection: true,
+  });
 
-	app.use(i18n);
+  app.use(i18n);
 };
