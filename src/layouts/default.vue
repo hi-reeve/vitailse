b5545a991ccb73b331b269578061a3072d7b3009
Christopher Reeve <reeveworkmail@gmail.com>
2021-10-21 13:46:16 +0700

    preview for vue-i18n bug

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,18 +1,20 @@
 <script setup lang="ts"></script>
 <template>
-	<header>
-		<app-header />
-	</header>
+    <header>
+        <app-header />
+    </header>
 
-	<main
-		class="
-			p-8
-			dark:bg-gray-800 dark:text-white
-			bg-white
-			text-gray-800
-			min-h-screen
-		"
-	>
-		<router-view />
-	</main>
+    <main
+        class="
+            p-8
+            dark:bg-gray-800 dark:text-white
+            bg-white
+            text-gray-800
+            min-h-screen
+        "
+    >
+        <router-view />
+    </main>
+
+    <periodic-update />
 </template>
