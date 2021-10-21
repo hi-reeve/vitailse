7f8393db41d55028b25b054812060b25d87c2e35
Christopher Reeve <reeveworkmail@gmail.com>
2021-10-21 14:07:44 +0700

    revert to yesterday commit

--- vite.config.ts
+++ vite.config.ts
@@ -11,100 +11,100 @@ import { VitePWA } from 'vite-plugin-pwa';
 import VueI18n from '@intlify/vite-plugin-vue-i18n';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    Components({
-      dts: true,
-      resolvers: [
-        IconsResolver({
-          prefix: 'icon',
-        }),
-      ],
-    }),
-    Icons({
-      compiler: 'vue3',
-    }),
-    AutoImport({
-      dts: true,
-      // targets to transform
-      include: [
-        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
-        /\.vue\??/, // .vue
-      ],
+	plugins: [
+		vue(),
+		Components({
+			dts: true,
+			resolvers: [
+				IconsResolver({
+					prefix: 'icon',
+				}),
+			],
+		}),
+		Icons({
+			compiler: 'vue3',
+		}),
+		AutoImport({
+			dts: true,
+			// targets to transform
+			include: [
+				/\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+				/\.vue\??/, // .vue
+			],
 
-      // global imports to register
-      imports: [
-        // presets
-        'vue',
-        'vue-router',
-        'vue-i18n',
-        '@vueuse/core',
-        '@vueuse/head',
-        // custom
-      ],
+			// global imports to register
+			imports: [
+				// presets
+				'vue',
+				'vue-router',
+				'vue-i18n',
+				'@vueuse/core',
+				'@vueuse/head',
+				// custom
+			],
 
-      // custom resolvers
-      // see https://github.com/antfu/unplugin-auto-import/pull/23/
-      resolvers: [],
-    }),
-    Pages(),
-    Layouts(),
-    VitePWA({
-      includeAssets: [
-        'favicon-16x16.png',
-        'favicon-32x32.png',
-        'favicon.ico',
-        'robots.txt',
-        'apple-touch-icon.png',
-      ],
-      manifest: {
-        name: 'Vitailse',
-        short_name: 'Vitailse',
-        description: 'Opinionated vite template with TailwindCSS',
-        theme_color: '#076AE0',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
-        ],
-      },
-    }),
-    VueI18n({
-      runtimeOnly: true,
-      compositionOnly: true,
-      include: [resolve(__dirname, 'locales/**')],
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-    },
-  },
-  server: {
-    fs: {
-      strict: true,
-    },
-  },
-  optimizeDeps: {
-    include: ['vue', 'vue-router', '@vueuse/core', '@vueuse/head'],
-    exclude: ['vue-demi'],
-  },
-  // @ts-ignore
-  ssgOptions: {
-    script: 'async',
-    formatting: 'minify',
-  },
+			// custom resolvers
+			// see https://github.com/antfu/unplugin-auto-import/pull/23/
+			resolvers: [],
+		}),
+		Pages(),
+		Layouts(),
+		VitePWA({
+			includeAssets: [
+				'favicon-16x16.png',
+				'favicon-32x32.png',
+				'favicon.ico',
+				'robots.txt',
+				'apple-touch-icon.png',
+			],
+			manifest: {
+				name: 'Vitailse',
+				short_name: 'Vitailse',
+				description: 'Opinionated vite template with TailwindCSS',
+				theme_color: '#076AE0',
+				icons: [
+					{
+						src: 'pwa-192x192.png',
+						sizes: '192x192',
+						type: 'image/png',
+					},
+					{
+						src: 'pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+					},
+					{
+						src: 'pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'any maskable',
+					},
+				],
+			},
+		}),
+		VueI18n({
+			runtimeOnly: true,
+			compositionOnly: true,
+			include: [resolve(__dirname, 'locales/**')],
+		}),
+	],
+	resolve: {
+		alias: {
+			'@': resolve(__dirname, './src'),
+		},
+	},
+	server: {
+		fs: {
+			strict: true,
+		},
+	},
+	optimizeDeps: {
+		include: ['vue', 'vue-router', '@vueuse/core', '@vueuse/head'],
+		exclude: ['vue-demi'],
+	},
+	// @ts-ignore
+	ssgOptions: {
+		script: 'async',
+		formatting: 'minify',
+	},
 });
